f770a853ff5508ec701085dcb53b16c01ddd1075
DuyB189 <bduy.le189@gmail.com>
2022-11-22 15:54:41 +0700

    22/11/2022 - 15h55

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
 import Textfield from '@atlaskit/textfield';
 import Button from '@atlaskit/button';
 import TodoList from './components/TodoList';
+import { useCallback, useEffect, useState } from 'react';
+import { v4 } from 'uuid';
+
+const TODO_APP_STORAGE_KEY = 'TODO_APP';
 
 function App() {
   // state, props 
 
+  //react hook - useState
 
+  const [todoList, setTodoList] = useState([]); // array - 2 element 
+  const [textInput, setTextInput] = useState(""); // string -  luu tru gia tri nguoi dung nhap vao input
+
+  useEffect(() => {
+    const storagedTodoList = localStorage.getItem(TODO_APP_STORAGE_KEY);
+    if (storagedTodoList) {
+      setTodoList(JSON.parse(storagedTodoList));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(TODO_APP_STORAGE_KEY, JSON.stringify(todoList));
+  }, [todoList]);
+
+  const onTextInputChange = useCallback((e) => {
+    setTextInput(e.target.value); //e.target.value - noi dung nguoi dung da nhap vo the INput
+  }, []);
+
+  const onAddBtnClick = useCallback((e) => {
+    //viet code de them text input vao todo list
+
+    setTodoList([
+      { id: v4(), name: textInput, isComplete: false },//doi vi tri 22-23
+      ...todoList,
+    ])
+
+    setTextInput("");
+  },
+    [textInput, todoList]
+  );
+
+  const onCheckBtnClick = useCallback((id) => {
+    setTodoList((prevState) =>
+      prevState.map((todo) =>
+        todo.id === id ? { ...todo, isCompleted: true } : todo
+      )
+    );
+  }, []);
 
   return (
     <>
       <h3>Danh sách việc cần làm !!!</h3>
       <Textfield
-        name="add-todo"
-        placeholder='Nhập công việc cần làm...'
+        name='add-todo'
+        placeholder='Thêm việc cần làm...'
         elemAfterInput={
-          <Button isDisabled={false} appearance='primary'>Thêm</Button>
+          <Button
+            isDisabled={!textInput}
+            appearance='primary'
+            onClick={onAddBtnClick}
+          >
+            Thêm
+          </Button>
         }
-        css={{ padding: '2px 4px 2px' }}
+        css={{ padding: "2px 4px 2px" }}
+        value={textInput}
+        onChange={onTextInputChange}
       ></Textfield>
-      <TodoList />
+      <TodoList todoList={todoList} onCheckBtnClick={onCheckBtnClick} />
     </>
   );
 }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,17 +1,47 @@
+import Button from "@atlaskit/button";
 import React from "react";
-import Button from '@atlaskit/button';
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import CheckIcon from "@atlaskit/icon/glyph/check";
 
 const ButtonStyled = styled(Button)`
-margin-top: 5px;
-text-align: left;
-justify-content: left;
+  margin-top: 5px;
+  text-align: left;
+  &,
+  &:hover {
+    ${(p) =>
+        p.isCompleted &&
+        css`
+        text-decoration: line-through;
+      `}
+  }
+  &:hover {
+    .check-icon {
+      display: inline-block;
+    }
+  }
+  .check-icon {
+    display: none;
+    &:hover {
+      background-color: #e2e2e2;
+      border-radius: 3px;
+    }
+  }
 `;
 
-export default function Todo() {
+export default function Todo({ todo, onCheckBtnClick }) {
     return (
-        <>
-            <ButtonStyled shouldFitContainer>Item1</ButtonStyled>
-        </>
+        <ButtonStyled
+            isCompleted={todo.isCompleted}
+            shouldFitContainer
+            iconAfter={
+                !todo.isCompleted && ( //khi isCompleted == false => hien ra checkicon
+                    <span className='check-icon' onClick={() => onCheckBtnClick(todo.id)}>
+                        <CheckIcon primaryColor="green" />
+                    </span>
+                )
+            }
+        >
+            {todo.name}
+        </ButtonStyled>
     );
 }
